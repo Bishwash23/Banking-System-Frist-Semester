4856448b09364e6d988ccd72a137c3b90ae3dc41
Bishwash23 <bishwashb23@gmail.com>
2024-04-27 16:25:55 +0000

    file to save the details

--- Malaysian Bank Service System.py
+++ Malaysian Bank Service System.py
@@ -1,3 +1,10 @@
 
 import os 
 from datetime import datetime
+
+# File to save the details
+SUPER_USER = "super_user.txt"
+ADMIN_STAFF = "admin_staff.txt"
+STAFF = "staff.txt"
+CUSTOMER = "customer.txt"
+TRANSACTION_FILE = "transaction.txt"
