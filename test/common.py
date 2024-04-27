435e0243e75dc2441d754e3c94a108e20f79948a
Bishwash23 <bishwashb23@gmail.com>
2024-04-27 16:21:13 +0000

    ok

--- test/common.py
+++
@@ -1,9 +0,0 @@
-print_date_time() # function to print date and time
-    
-# Example usage:
-username = input("Enter Username: ")
-password = input("Enter Password: ")
-if login_admin_staff(username, password):
-    print("Login successful.")
-else:
-    print("Invalid username or password.")
