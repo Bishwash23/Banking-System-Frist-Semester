428e5ec75f5727ab9e5d50df796a41274c31ebfd
Bishwash23 <bishwashb23@gmail.com>
2024-04-27 17:01:29 +0000

    adding some comments

--- Malaysian Bank Service System.py
+++ Malaysian Bank Service System.py
@@ -18,20 +18,24 @@ current_time = datetime.now()
 
 # Function to print date and time
 def print_date_time():
+    # Print the current date and time in the specified format
     print("\nDate:", current_time.strftime("%Y-%m-%d"))
     print("Time:", current_time.strftime("%I:%M:%S %p"))
 
 # Function to create a default Super User Account
 def create_super_user():
+    # Default super user credentials
     super_user_data = {
         "username": "admin",
         "password": "admin@123"
     }
+    # Check if the super user file does not exist
     if not os.path.exists(SUPER_USER):
         # If file does not exist, create it and write default super user details
         with open(SUPER_USER, 'w') as file:
             file.write(f"Username: {super_user_data['username']} \nPassword: {super_user_data['password']}")
     else:
+        # If the file exists, do nothing
         return
 create_super_user() # Call function to create the super user
 
@@ -40,14 +44,23 @@ def login_super_user(username, password):
     saved_username = None
     saved_password = None
     
+    # Open the super user file for reading
     with open(SUPER_USER, "r") as file:
+        # Iterate through each line in the file
         for line in file:
+            # Check if the line contains the username
             if line.strip().startswith("Username:"):
+                # Extract the saved username
                 saved_username = line.split(":")[1].strip()
+            # Check if the line contains the password
             elif line.strip().startswith("Password:"):
+                # Extract the saved password
                 saved_password = line.split(":")[1].strip()
-
+        
+        # Check if the provided username and password match the saved credentials
         if username == saved_username and password == saved_password:
+            # If they match, return True indicating successful authentication
             return True
         else:
+            # If they do not match, return False indicating authentication failure
             return False
